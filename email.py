800535b57c2cc4fa0cbbefbe7c96133092580db3
Creation-without-limits <ags.sinjal17@gmail.com>
2019-07-25 15:55:21 +0800

    Add files via upload

---
+++ email.py
@@ -0,0 +1,24 @@
+#
+# email.py - apam module - send email with HTML content.
+#
+#
+# See 'LICENSE' for module copying
+#
+
+import smtplib
+
+# Module global configs
+MODULE_AUTHOR = "apam <apam.banjar17@gmail.com>"
+MODULE_LICENSE = "GPLv3"
+MODULE_VERSION = "0.1"
+MODULE_DATE = "22-06-2016"
+# Keep it short
+MODULE_DE   = "Send an email with HTML content."
+
+class Email(object):
+    """ Send HTML email """
+    def __init__(self):
+        pass
+
+def main():
+    pass
---
+++ localhost.profile
@@ -0,0 +1,18 @@
+#
+# Default apam profile
+#
+
+# Website URL
+url = https://localhost
+
+# <from action>
+action_url = https://localhost
+
+# The port apam will listen
+port = 80
+
+# HTTP User-Agent
+user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36"
+
+# HTML file to load instead of URL
+# html_file = "file.html"
